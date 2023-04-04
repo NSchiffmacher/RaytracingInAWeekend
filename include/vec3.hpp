6f10b8e83a3454d66fe56da29f4eb387f0a1f0a4
NSchiffmacher <nathan.schiffmacher@gmail.com>
2023-04-04 21:54:36 +0200

    Initial commit

---
+++ include/vec3.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace raytracing
+{
+
+using vec3 = Eigen::Matrix<double, 3, 1>;
+// provides : .norm()
+// .squaredNorm()
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fmt/core.h>
+
+#include <color.hpp>
+// #include <vec3.hpp>
+
+using namespace raytracing;
+
+int main(){
+    const int width = 256;  
+    const int height = 256;
+
+    std::cout << "P3\n" << width << " " << height << "\n255\n";
+
+    std::cerr << "Starting..." << std::endl;
+    for (int y = height - 1; y >= 0; --y){
+        for (int x = 0; x < width; ++x) {
+            const auto c = color{
+                static_cast<double>(x) / (width),
+                static_cast<double>(y) / (height),
+                0.25
+            };
+            
+            write_color(std::cout, 256. * c);
+        }
+    }  
+    std::cerr << fmt::format("Wrote {} pixels", width*height) << std::endl;
+}
